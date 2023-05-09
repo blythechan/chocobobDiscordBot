8fc2d86bcb25c6ed4086c7d12a5ac604eb58cfb6
tolliverk <tolliverkl@etsu.edu>
2023-05-08 21:39:59 -0400

    Finishing ship

--- commands/bot/help.js
+++ commands/bot/help.js
@@ -6,57 +6,58 @@ const Paginate = require('discordjs-paginate');
     const BOT_EMBED = new EmbedBuilder()
         .setTitle(`Utility Commands`)
         .setColor(COLOR)
-        .setDescription(`Available utility commands`)
+        .setDescription(`<i>Available utility commands</i>`)
         .addFields(
             { name: 'Ping', value: 'Command `/ping`' },
             { name: ' ', value: 'Checks and displays latency between bot and client.' },
-            { name: ' ', value: 'Status: Finished.' },
+            { name: ' ', value: 'Status: :white_check_mark:' },
             { name: 'Purge Messages', value: 'Command `/purge user`' },
             { name: ' ', value: 'Removes up to 100 messages within the last 14 days on the current text-channel. User can be selected by the command itself. It is possible to select Chocobob. Success message will disappear after 10 seconds.' },
-            { name: ' ', value: 'Status: Finished.' },
+            { name: ' ', value: 'Status: :white_check_mark:' },
             { name: 'Verify Character', value: 'Command `/verify server user`' },
             { name: ' ', value: 'Assists with connecting and verifying a Discord user to their FFXIV Lodestone character. Used to determine roles, nicknames, and permissions within the server.' },
-            { name: ' ', value: '❌ Status: TBD.' },
+            { name: ' ', value: 'Status:❌ TBD.' },
             { name: 'FFXIV Community News', value: 'Command `/headlines`' },
             { name: ' ', value: 'TBD, may change.' },
-            { name: ' ', value: '❌ Status: TBD.' }
+            { name: ' ', value: 'Status: ❌ TBD' }
         );
     const ECONOMY_EMBED = new EmbedBuilder()
         .setTitle(`Economy Commands`)
         .setColor(COLOR)
-        .setDescription(`FFXIV Marketboard commands`)
+        .setDescription(`<i>FFXIV Marketboard commands</i>`)
         .addFields(
             { name: 'Market Board Search', value: 'Command `/mb item server`' },
-            { name: ' ', value: '❌ Status: API support is being researched (likely Uiniversalis).' }
+            { name: ' ', value: 'Status: ❌ API support is being researched (likely Uiniversalis).' }
         );
     const FREECOMPANY_EMBED = new EmbedBuilder()
         .setTitle(`Free Company Commands`)
         .setColor(COLOR)
-        .setDescription(`FFXIV Free Company statistics commands`)
+        .setDescription(`<i>FFXIV Free Company statistics commands</i>`)
         .addFields(
             { name: 'Free Company Statistics', value: 'Command `/fcstats fc server`' },
             { name: ' ', value: 'Lists Free Company data from FFFXIVAPI, and filters and paginates by 25 entries at a time. If two or more FCs share similar FC names, Chocobob will only retrieve the first result. Please be **specific**.' },
-            { name: ' ', value: '❌ Status: Pagination is being worked on. Additional API support is being researched.' }
+            { name: ' ', value: 'Status: ❌ Additional API support is being researched.' }
         );
     const FUN_EMBED = new EmbedBuilder()
         .setTitle(`Fun Commands`)
         .setColor(COLOR)
-        .setDescription(`Random commands`)
+        .setDescription(`<i>Random commands</i>`)
         .addFields(
             { name: 'Head Pats', value: 'Command `/headpats`' },
             { name: ' ', value: 'Makes Chocobob blush and feel loved for all his hard work.' },
-            { name: ' ', value: 'Status: Finished.' },
+            { name: ' ', value: 'Status: :white_check_mark:' },
             { name: 'Matchmaker', value: 'Command `/ship shipUserA shipUserB`' },
             { name: ' ', value: 'Jokingly determines a percentage of possibility between two users.' },
-            { name: ' ', value: '❌ Status: TBD.' }
+            { name: ' ', value: 'Status: :white_check_mark:' }
         );
     const DB_EMBED = new EmbedBuilder()
-        .setTitle(`Agreement from Developer`)
+        .setTitle(`About Chocobob Bot`)
         .setColor(COLOR)
-        .setDescription(`Purpose, expectations, promises, the whole shebang.`)
+        .setDescription(`<i>Purpose, expectations, promises, the whole shebang.</i>`)
         .addFields(
             { name: 'Purpose and Promise of Chocobob', value: 'A fun Discord bot with various commands to not only entertain a Discord user but assist a FFXIV user along their journey throughout Eorzea and beyond.' },
             { name: ' ', value: 'All actions made by Chocobob require either a command or a command ran by a server moderator. Any data saved is strictly to log the execution of an action and the response created by Chocobob. Message history that is unrelated to a command will not and will never be saved.' },
+            { name: 'Chocobob Bot Details', value: 'Chocobob#9508 is developed and maintained by Blythe Bleethe#6060. It relies heavily on FFXIVAPI, Lodestone API, and Dalamund Bridge. If you find that Chocobob is incorrectly using your art, is acting unpredictable, or could be improved, please DM Blythe Bleethe#6060.'}
         );
     const EMBEDS = [ BOT_EMBED, ECONOMY_EMBED, FREECOMPANY_EMBED, FUN_EMBED, DB_EMBED ];
     
@@ -66,6 +67,11 @@ const Paginate = require('discordjs-paginate');
             .setDescription('Lists commands.'),
         async execute(interaction) {
             // Send DM to the message's author
-            interaction.user.send({ content: `Curious about my Kweh Powers, are you? *DM sent because you are a member of a mutal server: ${interaction.member.guild.name}. Do not reply to this message as I will have returned to my Chocobo Stable once you finish reading this.*`, embeds: EMBEDS });
+            interaction.user.send({ content: `Curious about my Kweh Powers, are you? *DM sent because you are a member of a mutal server: ${interaction.member.guild.name}. Do not reply to this message as I will have returned to my Chocobo Stable once you finish reading this.*`, embeds: EMBEDS })
+                .then(sentMessage => interaction.reply(`I just sent you a DM about all my ~~Kwehbilities~~ commands!`))
+                .catch(error => {
+                    console.error(error.message);
+                    interaction.reply(`I couldn't send you a DM about my ~~Kwehbilities~~ commands... well, the Kupo are rather buys these days.`)
+                });
         },
     };
--- commands/fun/ship.js
+++ commands/fun/ship.js
@@ -1,18 +1,121 @@
-const { SlashCommandBuilder, roleMention, userMention } = require('discord.js');
+const { SlashCommandBuilder, roleMention, userMention, AttachmentBuilder } = require('discord.js');
+const Canvas = require('@napi-rs/canvas');
+
+const applyText = (canvas, text) => {
+	const context = canvas.getContext('2d');
+	let fontSize = 30;
+
+	do {
+		context.font = `${fontSize -= 10}px Arial`;
+	} while (context.measureText(text).width > canvas.width - 500);
+
+	return context.font;
+};
+
+const colorThreshold = (matchRate) => {
+        if(matchRate < 10) {
+                return '#ff0004';
+        } else if(matchRate < 20) {
+                return '#e8060a';
+        } else if(matchRate < 30) {
+                return '#c9080b';
+        } else if(matchRate < 40) {
+                return '#af0a0d';
+        } else if(matchRate < 50) {
+                return '#990a0d';
+        } else if(matchRate < 60) {
+                return '#870c0e';
+        } else if(matchRate < 70) {
+                return '#6b0a0c';
+        } else if(matchRate < 80) {
+                return '#490809';
+        } else if(matchRate < 90) {
+                return '#300607';
+        } else if(matchRate === 100) {
+                return 'black';
+        } 
+}
+
+const sayThis = (matchRate) => {
+        if(matchRate > 75) return "I ship it!";
+        if(matchRate < 35) return "That ship has long since sailed.";
+        return "";
+}
+
+const botId             = "1070431418750087342";
+const botOwnerId        = "139920603711930368";
+const images            = [
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.QoczFHiwSyQ9__9KPWXLwQHaEK%26pid%3DApi&f=1&ipt=a80c0516791905a0cad6ddfaaaa3845c5bbfc58b7f1692c28f54f6591bb47de2&ipo=images'
+]
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ship')
-		.setDescription('Ship a user in your server to another user in your server')
-        .addStringOption(option => option.setName('shipa').setDescription('Mention the first user').setRequired(true))
-        .addStringOption(option => option.setName('shipb').setDescription('Mention the second user').setRequired(true)),
+		.setDescription('Want to know how compatible users are in your server? Command will ping users involved.')
+                .addUserOption(option => option.setName('shipa').setDescription('Mention the first user').setRequired(true))
+                .addUserOption(option => option.setName('shipb').setDescription('Mention the second user').setRequired(true)),
 	async execute(interaction) {
-        const matchRate = Math.round(Math.random() * 99) + 1;
-        
-        const shipA = interaction.options.getString('shipa');
-        const shipB = interaction.options.getString('shipb');
-        //const shipA = interaction.user.displayAvatarURL();
-        const user = userMention(id);
-        await interaction.reply(`Test...`);
+                try {
+                        const shipA = interaction.options.getUser('shipa');
+                        const shipB = interaction.options.getUser('shipb');
+                        let matchRate = 0;
+                        if((shipB.id === botId || shipA.id === botId)) {
+                                if(shipB.id === botOwnerId || shipA.id === botOwnerId) {
+                                        matchRate = 100;
+                                } else {
+                                        matchRate = 0;
+                                }
+                        } else {
+                                matchRate = Math.round(Math.random() * 99) + 1;
+                        }
+
+                        const canvas = Canvas.createCanvas(880, 350);
+		        const context = canvas.getContext('2d');
+
+                        const background = await Canvas.loadImage(images[0]);
+
+                        context.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+                        const matchRateImage = await Canvas.loadImage('https://img.icons8.com/?size=512&id=23128&format=png');
+                        context.drawImage(matchRateImage, 200, 50, 150, 150);
+
+                        // Draw text
+                        context.font = applyText(canvas, `${shipB.username}`);
+                        context.fillStyle = '#020200';
+                        context.fillText(`${shipA.username}`, 70, 200);
+
+                        context.font = '30px Arial';
+                        context.fillStyle = colorThreshold(matchRate);
+                        const len = matchRate < 100
+                                ? 250
+                                : 240;
+                        context.fillText(`${matchRate}%`, len, 140);
+                        
+                        context.font = applyText(canvas, `${shipB.username}`);
+                        context.fillStyle = '#020200';
+                        context.fillText(`${shipB.username}`, 350, 200);
+
+                        // Make a circle
+                        context.beginPath();
+                        context.arc(130, 115, 65, 0, Math.PI * 2, true);
+                        context.arc(420, 115, 65, 0, Math.PI * 2, true);
+                        context.closePath();
+                        context.clip();
+
+                        const avatarShipA = await Canvas.loadImage(shipA.displayAvatarURL({ extension: 'jpg' }));
+                        const avatarShipB = await Canvas.loadImage(shipB.displayAvatarURL({ extension: 'jpg' }));
+
+                        // Draw a shape onto the main canvas
+                        context.drawImage(avatarShipA, 50, 50, 145, 145);
+                        context.drawImage(avatarShipB, 350, 50, 145, 145);
+
+                        // Use the helpful Attachment class structure to process the file for you
+                        const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'shipping.png' });
+                        const botReply = sayThis(matchRate);
+                        await interaction.reply({ content:`Hmm... shipping ${shipA} and ${shipB}? ${botReply}`, files: [attachment] });
+                } catch (ex) {
+                        console.error(ex.message);
+                        await interaction.reply('Sorry, got distracted... ask me that again.');
+                }
 	},
 };
