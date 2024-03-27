a61a1a10239effc69290d1039414c66c900b97a7
tolliverk <tolliverkl@etsu.edu>
2024-03-26 23:21:36 -0400

    Remove unused file

--- src/commands/bot/help.js
+++ src/commands/bot/help.js
@@ -10,7 +10,7 @@ module.exports = {
     const EMBED = customEmbedBuilder(
         `Help is only a click away kweh!`,
         defaults.CHOCO_HAPPY_ICON,
-        `My ~~kwehbilities~~ abilities are vast. Please check them out at: ${"https://github.com/hartleyk1/chocobobDiscordBot/blob/main/README.md"}`,
+        `My ~~kwehbilities~~ abilities are vast. Please check them out at: ${"https://github.com/hartleyk1/chocobobDiscordBot/blob/main/commands.md"}`,
         [
             { name: "Terms", value: "A fun Discord bot with various commands to not only entertain a Discord user but also assist a FFXIV user along their journey throughout Eorzea and beyond." },
             { name: " ", value: "All actions made by Chocobob require either a command or a command ran by a server moderator. Any data saved is strictly to log the execution of an action and the response created by Chocobob. Message history that is unrelated to a command will not and will never be saved." },
